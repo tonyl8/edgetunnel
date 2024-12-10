37a61d245c09c90fcff324ad0cfbb9d09e1be91d
tonyl8 <tonylewis5920@gmail.com>
2024-12-10 21:11:14 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,11 +1,11 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '71a1386c-9e5d-459b-b50a-88ecc2d5769d';
 let proxyIP = '';
-let sub = '';
+let sub = 'VLESS.fxxk.dedyn.io';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
-let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
+let subConfig = "https://raw.githubusercontent.com/gtang8/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
 let socks5Address = '';
